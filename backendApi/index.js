52841d83f638d2df64f4d6213f345f02096e091e
Jorge Cuervo <jorgecuervo527@gmail.com>
2026-09-14 18:56:55 -0500

    cambios para subir a la nube

--- backendApi/index.js
+++ backendApi/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-var port = process.env.PORT;
+var port = process.env.PORT || 5000;
 
 getConnection();
 
